66c44c579fc35950604e0a1116406864447d19d2
Alexis Pizarro <alexpizarro3@gmail.com>
2022-07-24 22:20:57 -0600

    First Login Menu

--- src/Screens/Login.js
+++ src/Screens/Login.js
@@ -1,8 +1,49 @@
+import TextField from '@mui/material/TextField';
+import { styled } from '@mui/material/styles';
+import { Grid, Button } from '@mui/material';
+
 export default function Login() {
   return (
-    <div>
-      Login
-    </div>
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      marginTop="6%"
+      style={{ minHeight: '100vh' }}
+    >
+      <Grid item xs={3}>
+        <CssTextField label="Custom CSS" id="custom-css-outlined-input" variant="filled" sx={{ width: 300 }} />
+        <br />
+        <br />
+        <TextField id="filled-basic" label="Password" variant="filled" sx={{ width: 300 }} />
+        <br />
+        <br />
+        <Button variant="contained" color="primary" sx={{ width: 300 }}>Login</Button>
+      </Grid>
+    </Grid>
   )
 }
+
+const CssTextField = styled(TextField)({
+  '& label.Mui-focused': {
+    color: 'blue',
+  },
+  '& .MuiInput-underline:after': {
+    borderBottomColor: 'green',
+  },
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      borderColor: 'black',
+    },
+    '&:hover fieldset': {
+      borderColor: 'brown',
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: 'blue',
+    },
+  },
+});
+
+
 
