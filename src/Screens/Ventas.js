82ab6e5ef850546dcab85a848942de01ba6490e4
Alexis Pizarro <alexpizarro3@gmail.com>
2022-08-13 23:10:17 -0600

    llenar el gridview con cada item

--- src/Screens/Ventas.js
+++ src/Screens/Ventas.js
@@ -9,48 +9,17 @@ import { obtenerProductos } from '../Components/Api';
 import MenuItem from '@mui/material/MenuItem';
 
 const columns = [
-  { field: 'idProducto', headerName: 'Id', width: 90, headerAlign: 'center', },
+  { field: 'id', headerName: 'Id', width: 50, headerAlign: 'center', },
+  { field: 'idProducto', headerName: 'Id Producto', width: 100, headerAlign: 'center', },
+  { field: 'Nombre', headerName: 'Producto', width: 275, editable: true, },
+  { field: 'Cantidad', headerName: 'Cantidad', type: 'number', width: 150, editable: true, },
+  { field: 'PrecioVenta', headerName: 'Precio de Venta', type: 'number', width: 200, editable: true, },
+  { field: 'SubTotal', headerName: 'SubTotal', description: 'Subtotal de Multiplicar Cantidad x Precio', type: 'number', width: 150, editable: true, },
   {
-    field: 'Nombre',
-    headerName: 'Producto',
-    width: 150,
-    editable: true,
-  },
-  {
-    field: 'Cantidad',
-    headerName: 'Cantidad',
-    type: 'number',
-    width: 150,
-    editable: true,
-  },
-  {
-    field: 'Precio Venta',
-    headerName: 'Precio de Venta',
-    type: 'number',
-    width: 200,
-    editable: true,
-  },
-  {
-    field: 'SubTotal',
-    headerName: 'SubTotal',
-    description: 'Subtotal de Multiplicar Cantidad x Precio',
-    type: 'number',
-    width: 150,
-    editable: true,
-  },
-  {
-    field: 'iva',
-    headerName: 'IVA',
-    description: 'Multiplicar Subtotal * 13%',
-    type: 'number',
-    width: 150,
+    field: 'iva', headerName: 'IVA', description: 'Multiplicar Subtotal * 13%', type: 'number', width: 150,
     valueGetter: (params) =>
-      `${params.row.subtotal}` * 0.13,
+      `${params.row.SubTotal}` * 0.13,
   },
-];
-
-const rows = [
-
 ];
 
 const Ventas = () => {
@@ -59,13 +28,13 @@ const Ventas = () => {
   const [subtotal, setSubTotal] = useState(0);
   const [impuesto, setImpuesto] = useState(0);
   const [listProd, setListProd] = useState([]);
+  const [rows, setRows] = useState([]);
   const [idProducto, setIdProducto] = useState("");
+  const [desProducto, setDesProducto] = useState("");
   const [prProducto, setPrProducto] = useState(0);
   const [subTotalItem, setSubTotalItem] = useState(0);
   const [cantItem, setCantItem] = useState("");
   const textCantidad = useRef(null);
-
-  console.log(userContext);
 
   const fetchData = async () => {
     const loaddata = await obtenerProductos();
@@ -86,8 +55,9 @@ const Ventas = () => {
   };
 
   const prodSelect = (id) => {
-    let selected = listProd.find(prod =>  prod.IdProducto === id);
+    let selected = listProd.find(prod => prod.IdProducto === id);
     setPrProducto(selected.PrecioVenta);
+    setDesProducto(selected.Nombre);
     focus();
   };
 
@@ -99,15 +69,21 @@ const Ventas = () => {
     setCantItem(e.target.value);
   };
 
-
-
   const prcantChange = () => {
     const res = prProducto * cantItem;
     setSubTotalItem(res);
   };
 
-  console.log(subTotalItem);
-  
+  let idCounter = 0;
+  const createNewRow = () => {
+    idCounter = rows.length + 1; //aumenta el lenght del arreglo para asignar el nuevo id del gridview item
+    return { id: idCounter, idProducto: idProducto, Nombre: desProducto, Cantidad: cantItem, PrecioVenta: prProducto, SubTotal: subTotalItem };
+  };
+
+  const handleAddRow = () => { //funcion que agrega una fila nueva al grid usando los states de cada variable
+    setRows((prevRows) => [...prevRows, createNewRow()]); //
+  };
+
   return (
     <Box sx={{}}>
       <Typography variant="h1" component="div" gutterBottom sx={{ flexGrow: 1, fontSize: '25px', textAlign: 'center', color: 'white', bgcolor: "rgba(38, 7, 1, 0.6)", borderRadius: 1 }}>
@@ -136,7 +112,7 @@ const Ventas = () => {
           }
         }}
           sx={{ width: 125, margin: "0.5rem", "& label": { color: "black", fontSize: "14px" }, bgcolor: "orange", borderRadius: 1, boxShadow: 10 }} />
-        <Button variant='contained' sx={{ margin: "0.5rem", height: "3.5rem", width: "5.5rem", }} >Agregar</Button>
+        <Button variant='contained' onClick={handleAddRow} sx={{ margin: "0.5rem", height: "3.5rem", width: "5.5rem", }} >Agregar</Button>
         <Button variant='contained' sx={{ margin: "0.5rem", height: "3.5rem", width: "5.5rem", bgcolor: "black" }} >Cancelar</Button>
         <Button variant='contained' sx={{ margin: "0.5rem", height: "3.5rem", width: "5.5rem", bgcolor: "green" }} >Facturar</Button>
       </Box>
